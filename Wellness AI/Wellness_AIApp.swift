96f1dc753f5e1fb128db1c6ba839cbc3dd413491
Lucas Cipolla <lucascc2000@hotmail.com>
2025-10-13 18:15:23 -0300

    Initial Commit

---
+++ Wellness AI/Wellness_AIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wellness_AIApp.swift
+//  Wellness AI
+//
+//  Created by Lucas Cipolla on 13/10/25.
+//
+
+import SwiftUI
+
+@main
+struct Wellness_AIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
